ed786e890ddcfec2cc5e5a0595b612bfd6968eed
Masha0701 <reflection2008@gmail.com>
2022-12-31 23:04:21 +0300

    feat: loops added

--- src/06-promises-tasks.js
+++ src/06-promises-tasks.js
@@ -28,8 +28,28 @@
  *      .catch((error) => console.log(error.message)) // 'Error: Wrong parameter is passed!
  *                                                    //  Ask her again.';
  */
-function willYouMarryMe(/* isPositiveAnswer */) {
-  throw new Error('Not implemented');
+
+function willYouMarryMe(isPositiveAnswer) {
+  if (typeof isPositiveAnswer !== 'boolean') {
+    return 'Wrong parameter is passed! Ask her again.';
+  }
+
+  return new Promise((resolve) => {
+    resolve(isPositiveAnswer);
+  })
+    .then((res) => {
+      let answer;
+      if (typeof res !== 'boolean') {
+        answer = 'Wrong parameter is passed! Ask her again.';
+      }
+      if (res) {
+        answer = 'Hooray!!! She said "Yes"!';
+      } else {
+        answer = 'Oh no, she said "No".';
+      }
+      return answer;
+    })
+    .catch(() => 'Wrong parameter is passed! Ask her again.');
 }
 
 
--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -27,8 +27,20 @@
  *  21 => 'Fizz'
  *
  */
-function getFizzBuzz(/* num */) {
-  throw new Error('Not implemented');
+function getFizzBuzz(num) {
+  if (num % 3 === 0 && num % 5 === 0) {
+    return 'FizzBuzz';
+  }
+
+  if (num % 3 === 0) {
+    return 'Fizz';
+  }
+
+  if (num % 5 === 0) {
+    return 'Buzz';
+  }
+
+  return num;
 }
 
 
@@ -43,8 +55,13 @@ function getFizzBuzz(/* num */) {
  *   5  => 120
  *   10 => 3628800
  */
-function getFactorial(/* n */) {
-  throw new Error('Not implemented');
+function getFactorial(n) {
+  let res = n;
+  for (let i = 1; i < n; i += 1) {
+    res *= (n - i);
+  }
+
+  return res;
 }
 
 
@@ -60,10 +77,22 @@ function getFactorial(/* n */) {
  *   5,10  =>  45 ( = 5+6+7+8+9+10 )
  *   -1,1  =>  0  ( = -1 + 0 + 1 )
  */
-function getSumBetweenNumbers(/* n1, n2 */) {
-  throw new Error('Not implemented');
-}
 
+function getSumBetweenNumbers(n1, n2) {
+  let res = n1;
+
+  let num = n2 - n1;
+
+  if (n1 < 0 && n2 > 0) {
+    num += 1;
+  }
+
+  for (let i = 0; i < num; i += 1) {
+    res += Math.abs(n1 + i);
+  }
+
+  return res + n1;
+}
 
 /**
  * Returns true, if a triangle can be built with the specified sides a, b, c
@@ -80,8 +109,8 @@ function getSumBetweenNumbers(/* n1, n2 */) {
  *   10,1,1   =>  false
  *   10,10,10 =>  true
  */
-function isTriangle(/* a, b, c */) {
-  throw new Error('Not implemented');
+function isTriangle(a, b, c) {
+  return ((a + b) > c) && ((a + c) > b) && ((b + c) > a);
 }
 
 
@@ -117,8 +146,20 @@ function isTriangle(/* a, b, c */) {
  *   { top:20, left:20, width: 20, height: 20 }    =>  false
  *
  */
-function doRectanglesOverlap(/* rect1, rect2 */) {
-  throw new Error('Not implemented');
+function doRectanglesOverlap(rect1, rect2) {
+  const rect1Right = rect1.left + rect1.width;
+  const rect1Bottom = rect1.top + rect1.height;
+  const rect2Right = rect2.left + rect2.width;
+  const rect2Bottom = rect2.top + rect2.height;
+
+  return (rect2.left >= rect1.left
+      && rect2.top >= rect1.top
+      && rect2.left <= rect1Right
+      && rect2.top <= rect1Bottom)
+    || (rect1.left >= rect2.left
+      && rect1.top >= rect2.top
+      && rect1.left <= rect2Right
+      && rect1.top <= rect2Bottom);
 }
 
 
